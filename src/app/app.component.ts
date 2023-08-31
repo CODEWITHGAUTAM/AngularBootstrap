7cd00e7f6f438fb62a8ffbfc4bf51eb21068879d
CODEWITHGAUTAM <gautamsingh7424@gmail.com>
2023-08-31 15:11:01 +0530

    hggh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Reusable Component';
-  userDetails=[
-    {
-      name:'Anil Sidhu',email:'anil@test.com'
-
-
-    },{
-      name:'Gautam Singh Rajput',email:'gautam@rajput.com'
-    },
-    {
-      name:'Raushan Singh Rajput',email:'raushan@rajput.com'
-    },
-    {
-      name:'Love Singh Rajput',email:'love@rajput.com'
-    }
-  ]
-
+  title = 'Two way Binding';
+  name:any;
   
-
-
   
 
 }
